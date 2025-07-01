ac42220aace2e93b246fbc6091fddc3917e34d4f
be-engineer <41234995@qq.com>
2025-07-02 02:04:39 +0800

    添加package源

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+###
+ # @Author       : Leon Lee
+ # @Date         : 2025-07-02 00:20:02
+ # @LastEditors  : Leon
+ # @LastEditTime : 2025-07-02 02:03:21
+ # @Description  : file content
+ # @FilePath     : /mi-ac2100-immortalwrt/diy-part2.sh
+### 
 #============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
@@ -9,7 +17,10 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i 's/OpenWrt/Newifi-D1/g' package/base-files/files/bin/config_generate
+
+# 添加 kwrt package 源（避免重复添加）
+grep -q "src-git kwrt" feeds.conf.default || echo "src-git kwrt https://github.com/be-engineer/kwrt-packages" >> feeds.conf.default
